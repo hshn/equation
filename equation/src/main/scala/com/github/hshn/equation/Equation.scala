140677878a8ef1e385610f4c89e500ac75551682
Shota Hoshino <sht.hshn@gmail.com>
2016-03-27 17:31:29 +0900

    Use Rational instead of Double

--- equation/src/main/scala/com/github/hshn/equation/Equation.scala
+++ equation/src/main/scala/com/github/hshn/equation/Equation.scala
@@ -1,15 +1,17 @@
 package com.github.hshn.equation
 
-case class Equation(values: Double*) {
-  def apply(index: Int): Double = values(index)
-  def /(value: Double): Equation = map(_.map(_ / value))
-  def *(value: Double): Equation = map(_.map(_ * value))
+import spire.math._
+
+case class Equation(values: Rational*) {
+  def apply(index: Int): Rational = values(index)
+  def /(value: Rational): Equation = map(_.map(_ / value))
+  def *(value: Rational): Equation = map(_.map(_ * value))
   def -(other: Equation): Equation = map { values =>
     other.values.zipWithIndex.map {
       case (value, index) => values(index) - value
     }
   }
-  def map(f: Seq[Double] => Seq[Double]): Equation = Equation(f(values):_*)
+  def map(f: Seq[Rational] => Seq[Rational]): Equation = Equation(f(values):_*)
 
   override def toString: String = s"Equation(${values.mkString(", ")})"
 }
